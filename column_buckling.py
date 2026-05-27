2021769aa12d13ef4e244b4e2c91e596296ea226
mahdeotman9-commits <mahdeotman9@gmail.com>
2026-05-27 12:07:19 +0300

    Update column_buckling.py

--- column_buckling.py
+++ column_buckling.py
@@ -1,17 +1,4 @@
-def find_critical_load(L, E, A, r, c, e, sigma_allow):
-    """
-    L: אורך במ"מ
-    E: מודול אלסטיות ב-MPa
-    A: שטח חתך בממ"ר
-    r: רדיוס אינרציה במ"מ
-    c: מרחק לסיב קיצוני במ"מ
-    e: אקסצנטריות במ"מ
-    sigma_allow: מאמץ מותר ב-MPa
-    
-    Return: העומס P בניוטון (float)
-    """
-    
-    def f(P):
+def f(P):
         if P <= 0:
             return -sigma_allow
         
